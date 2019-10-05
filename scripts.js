2a9c0fa2e9e09d26c991de8f63c26a6cc4f3e558
Raeann Miller <raeann.miller@student.swic.edu>
2019-10-04 20:53:44 -0500

    Listen for a submit event on the form.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,35 @@
-// Enter a description of this program here.
+// Simple arithmetic calculator app
 
 /**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
+ *
+ *
  */
+
+function add(a, b) {
+	return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+function remainder(a, b) {
+	return a % b;
+}
+
+const form = document.querySelector("form")
+
+// Use an anonymous callback function as the 2nd argument
+
+form.addEventListener("submit", function(event){
+  // Prevent the default browser behavior - DONT SUBMIT
+  event.preventDefault();
+});
