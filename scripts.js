1291b8adba7e6b98d9753cd73b2518d85590663e
Raeann Miller <raeann.miller@student.swic.edu>
2019-10-04 21:04:20 -0500

    Log the value of the select and inputs

--- scripts.js
+++ scripts.js
@@ -26,10 +26,15 @@ function remainder(a, b) {
 }
 
 const form = document.querySelector("form")
-
+const num1 = document.querySelector("#num-1")
+const num2 = document.querySelector("#num-2")
+const select = document.querySelector("select")
 // Use an anonymous callback function as the 2nd argument
 
 form.addEventListener("submit", function(event){
   // Prevent the default browser behavior - DONT SUBMIT
   event.preventDefault();
+
+  // Form inputs have a property called value
+  console.log(num1.value, num2.value, select.value);
 });
