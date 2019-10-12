ada0989d773157e7258a94d8832a88d47d4fb261
Raeann Miller <raeann.miller@student.swic.edu>
2019-10-12 16:55:26 -0500

    Move Global Functions into an Objecet Literal

--- scripts.js
+++ scripts.js
@@ -1,37 +1,41 @@
+/* eslint-disable no-undef */
 // Simple arithmetic calculator app
 
-/**
- *
- *
- */
+// 2. Use BRACKET NOTATION to INVOKE the APPROPRIATE METHOD
 
-function add(a, b) {
-	return Number(a) + Number(b);
-}
+// eslint-disable-next-line no-unused-vars
+const calculator = {
+  add(a, b) {
+    return Number(a) + Number(b);
+  },
 
-function subtract(a, b) {
-	return a - b;
-}
+  subtract(a, b) {
+    return a - b;
+  },
 
-function multiply(a, b) {
-	return a * b;
-}
+  multiply(a, b) {
+    return a * b;
+  },
 
-function divide(a, b) {
-	return a / b;
-}
+  divide(a, b) {
+    return a / b;
+  },
 
-function remainder(a, b) {
-	return a % b;
-}
+  remainder(a, b) {
+    return a % b;
+  }
+};
 
-const form = document.querySelector("form")
-const num1 = document.querySelector("#num-1")
-const num2 = document.querySelector("#num-2")
-const select = document.querySelector("select")
+console.log(calcultor["add"](1, 5));
+
+const form = document.querySelector("form");
+const num1 = document.querySelector("#num-1");
+const num2 = document.querySelector("#num-2");
+const select = document.querySelector("select");
+
 // Use an anonymous callback function as the 2nd argument
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function(event) {
   // Prevent the default browser behavior - DONT SUBMIT
   event.preventDefault();
 
